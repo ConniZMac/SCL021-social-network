9b39e6dd3bf382bc81ead0697d1daa2734457ee6
cataelisa <catalinamoralesmedina@gmail.com>
2022-09-15 12:41:32 -0300

    commit obligatorio para hacer pull

--- src/router/routes.js
+++ src/router/routes.js
@@ -17,17 +17,12 @@
   
   function navigate(route) { //, substr = ""
       console.log(route)
-      const template = routes[route]?.template;
-      //const addEvents =  routes[route].addEvents
-      //const path = routes[route].path; 
+      const template = routes[route].template;
       history.pushState({}, route); //, `${path}?${substr}`
       const root = document.getElementById('root');
       root.innerHTML = '';
-      //const element = document.createElement('div');
-      //element.innerHTML=template;
-      //console.log({template});
       root.appendChild(template);//element
-      //addEvents()
+     
     }
     
     export { navigate, routes};
